e9c598f41e996b8e66880b201dbee2c4475b09fb
oyuyes <oyuyocalvin@gmail.com>
2025-05-25 21:51:12 +0300

    Removed old handlers, updated start_handler and main.py, added new utils folder

--- handlers/start_handler.py
+++ handlers/start_handler.py
@@ -1,14 +1,4 @@
-from telegram import Update
-from telegram.ext import CommandHandler, ContextTypes
 from utils.constants import MAIN_MENU
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    message = (
-        "👑 *Welcome to TIPS MANSA VIP!*\n\n"
-        "🔥 We deliver *2+ odds daily*, carefully selected to *double your money*. 💸\n"
-        "📊 Our betting tips are premium, researched, and trusted.\n\n"
-        "👇 Tap a button below to begin your winning journey!"
-    )
-    await update.message.reply_text(message, reply_markup=MAIN_MENU, parse_mode="Markdown")
-
-start_handler = CommandHandler("start", start)
+def start_handler():
+    print(MAIN_MENU)
